84fc504e28224eea984164ca6930773dc952f100
moonjason <jasonmoonjm@gmail.com>
2022-05-15 21:09:01 -0700

    redo

--- src/components/Home/Promo-1.js
+++
@@ -1,9 +0,0 @@
-function Promo1() {
-
-    return (
-      <div id="promo-container-1">
-       </div>
-    );
-  } 
-
-export default Promo1
--- src/components/Home/Promo-2.js
+++
@@ -1,9 +0,0 @@
-function Promo2() {
-
-    return (
-      <div id="promo-container-2">
-       </div>
-    );
-  } 
-
-export default Promo2
